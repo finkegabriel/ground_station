0f0431110060e65cb0d79718d3c0fae649d919b7
Gabe <finkegabriel@gmail.com>
2020-06-02 22:33:32 -0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-var spawn = require("child_process").spawn; 
-var process = spawn('python',["./main.py"]); 
+const express = require('express')
+const app = express()
+const port = 3000;
 
-process.stdout.on('data', function(data) { 
-    console.log(data.toString());
-} ) 
+let satDat = {};
+
+app.get('/sats', (req, res) =>
+    res.send('good'));
+
+app.get('/tle', (req, res) => {
+    var spawn = require("child_process").spawn;
+    var process = spawn('python', ["./main.py"]);
+    process.stdout.on('data', function (data) {
+        console.log(data.toString());
+        satDat = {tle:data.toString()};
+        return satDat;
+
+    });
+    res.send(satDat);
+});
+
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
